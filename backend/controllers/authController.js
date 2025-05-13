4f5efa3ec2bad70da024e8f130c34eae9245bec0
anshul <anshulgupta2028@gmail.com>
2025-05-14 00:45:51 +0530

    new changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -68,6 +68,8 @@ export const sendOtpController = async (req, res) => {
     } catch (twilioError) {
       logger.error(LOG_MESSAGES.TWILIO_ERROR(twilioError.message));
       //return failed to send otp
+      console.log("twilioError-->", twilioError);
+
       res.status(500).json({
         success: false,
         message: RESPONSE_MESSAGES.FAILED_TO_SEND_OTP,
